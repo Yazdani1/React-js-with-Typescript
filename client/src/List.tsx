f7c6040b685fd05b32b707284770e470359505ae
Yazdani <yaz4dani@gmail.com>
2022-03-06 23:01:56 +0200

    show api data

--- client/src/List.tsx
+++ client/src/List.tsx
@@ -3,7 +3,17 @@ import React, { useState, useEffect } from "react";
 const List = () => {
   const [showdata, setShowdata] = useState([]);
 
-  const loadData = () => {};
+  const API_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+
+  const loadData = () => {
+    fetch(API_ENDPOINT, {
+      method: "GET",
+    })
+      .then((result) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return <div>List</div>;
 };
