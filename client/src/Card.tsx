c9b35534d3f2735cddb493d90f811c61c732e3d1
Yazdani <yaz4dani@gmail.com>
2022-03-09 20:41:46 +0200

    decrese price

--- client/src/Card.tsx
+++ client/src/Card.tsx
@@ -3,6 +3,13 @@ import React, { useEffect, useState } from "react";
 const Card = () => {
   const [price, setPrice] = useState(0);
 
+  const increasePrice = ()=>{
+      setPrice(price+1);
+  }
+  const decresePrice = ()=>{
+      setPrice(price-1);
+  }
+
   return (
     <React.Fragment>
       <div>
