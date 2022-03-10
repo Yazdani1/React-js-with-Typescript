a2bb7add39a225ba1b155ac3259d3cec7f026102
Yazdani <yaz4dani@gmail.com>
2022-03-10 22:23:49 +0200

    card design

--- client/src/Card.tsx
+++ client/src/Card.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
+import "./card.css";
 
 const Card = () => {
   const [price, setPrice] = useState(0);
 
-  const increasePrice = ()=>{
-      setPrice(price+1);
-  }
-  const decresePrice = ()=>{
-      setPrice(price-1);
-  }
+  const increasePrice = () => {
+    setPrice(price + 1);
+  };
+  const decresePrice = () => {
+    setPrice(price - 1);
+  };
 
   return (
     <React.Fragment>
-      <div>
+      <div className="card-design">
         <p>Card View</p>
         <p onClick={() => setPrice(price + 5)}>{price}</p>
       </div>
