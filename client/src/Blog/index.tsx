c838a1da21b1579229956c72f1e74cf782f8f27e
Yazdani <yaz4dani@gmail.com>
2022-03-15 20:52:38 +0200

    blog props

--- client/src/Blog/index.tsx
+++ client/src/Blog/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Blogdata } from './types'
 
-const Blog:React.FC<Blogdata> = () => {
+const Blog:React.FC<Blogdata> = ({title,des,date,category,location}) => {
   return (
     <div>Blog</div>
   )
