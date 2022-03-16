e95c9e14e17e8af6ba780b0ff216b95cd11199c3
Yazdani <yaz4dani@gmail.com>
2022-03-16 22:24:37 +0200

    design blog list

--- client/src/Blog/index.tsx
+++ client/src/Blog/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Blogdata } from './types'
+import React from "react";
+import { Blogdata } from "./types";
 
-const Blog:React.FC<Blogdata> = ({title,des,date,category,location}) => {
+const Blog: React.FC<Blogdata> = ({ title, des, date, category, location }) => {
   return (
     <React.Fragment>
-      <div className='bloglist'>
-
+      <div className="bloglist">
+        <h3>{title}</h3>
+        <p>{des}</p>
+        <p>{date}</p>
+        <p>{category}</p>
+        <p>{location}</p>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
