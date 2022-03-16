0ee0e44c271782b5b5fbc9075688f15b337b9de8
Yazdani <yaz4dani@gmail.com>
2022-03-16 22:13:02 +0200

    get blog list

--- client/src/Blog/index.tsx
+++ client/src/Blog/index.tsx
@@ -3,7 +3,11 @@ import { Blogdata } from './types'
 
 const Blog:React.FC<Blogdata> = ({title,des,date,category,location}) => {
   return (
-    <div>Blog</div>
+    <React.Fragment>
+      <div className='bloglist'>
+
+      </div>
+    </React.Fragment>
   )
 }
 
